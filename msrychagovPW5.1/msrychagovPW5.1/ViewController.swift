ed30863232fb0385457ae1d6efb454b796b259fc
msrychagov <144157355+msrychagov@users.noreply.github.com>
2025-03-24 01:59:23 +0300

    Delete StoryBoard

---
+++ msrychagovPW5.1/msrychagovPW5.1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  msrychagovPW5.1
+//
+//  Created by Михаил Рычагов on 24.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
